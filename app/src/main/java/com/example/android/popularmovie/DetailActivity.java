fae27c90d795403c667d38781762ecee93afe1b7
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-10-10 11:20:12 +0900

    Update onOptionsItemSelected in DetailActivity.java

--- app/src/main/java/com/example/android/popularmovie/DetailActivity.java
+++ app/src/main/java/com/example/android/popularmovie/DetailActivity.java
@@ -78,10 +78,12 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
